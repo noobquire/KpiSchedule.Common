e8044dc99884708f55b5c8ef45c4202c0189889d
Oleksii Lytvynov <ostrich.alexey@gmail.com>
2023-02-14 19:50:11 +0200

    Rename Scraper to SampleScraper

--- KpiSchedule.SampleScraper/Program.cs
+++ KpiSchedule.SampleScraper/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using KpiSchedule.Common.Clients;
+﻿using KpiSchedule.Common.Clients;
 using KpiSchedule.Common.Exceptions;
 using KpiSchedule.Common.Models.RozKpiApi;
 using KpiSchedule.Common.ServiceCollectionExtensions;
